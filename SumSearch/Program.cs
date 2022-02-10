771a4fadc4eb8d60e353a994605f1d11d3378cff
Ulf94 <79094141+Ulf94@users.noreply.github.com>
2022-02-10 10:49:07 +0100

    First release

---
+++ SumSearch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var searcher = new SumSearchC();
+            var number = new List<int>() { 10,15,3,7};
+            var k = 17;
+            Console.WriteLine(searcher.IsSumExist(number, k));
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ SumSearch/SumSearchC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumSearch
+{
+    internal class SumSearchC
+    {
+        public bool IsSumExist(List<int> number, int k)
+        {
+            for(int i=0; i<number.Count; i++)
+            {
+                for(int j=i+1; j < number.Count; j++)
+                {
+                    if(number[i] + number[j] == k) return true;
+                }
+            }
+            return false;
+        }
+    }
+}
